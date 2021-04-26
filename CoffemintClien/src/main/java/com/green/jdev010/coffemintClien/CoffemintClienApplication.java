46c50711405133a3d23a1e65fc1e6f4b8ea7d962
baobap79 <baonse140178@fpt.edu.vn>
2021-04-26 20:13:21 +0700

    + initialize Source

---
+++ CoffemintClien/src/main/java/com/green/jdev010/coffemintClien/CoffemintClienApplication.java
@@ -0,0 +1,13 @@
+package com.green.jdev010.coffemintClien;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffemintClienApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoffemintClienApplication.class, args);
+	}
+
+}
---
+++ CoffemintClien/src/test/java/com/green/jdev010/coffemintClien/CoffemintClienApplicationTests.java
@@ -0,0 +1,13 @@
+package com.green.jdev010.coffemintClien;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffemintClienApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
